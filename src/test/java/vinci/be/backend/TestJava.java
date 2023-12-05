94092aef5406810ea3ca6fed92a00c9c548e42f5
juliendec <juliendecoen2308@gmail.com>
2023-12-05 14:53:20 +0100

    test with entity

--- src/test/java/vinci/be/backend/TestJava.java
+++ src/test/java/vinci/be/backend/TestJava.java
@@ -1,22 +1,13 @@
 package vinci.be.backend;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /** The Spring Cicd Demo Application Tests. */
-@SpringBootTest(classes = HelloWorldService.class)
+@SpringBootTest(classes = TextHelloWorld.class)
 class TestJava {
-
-    @Mock
-    private HelloWorldControler priceProxy;
-
-    @InjectMocks
-    private HelloWorldService matchingService;
-
     @Test
     void contextLoads() {
         assertEquals(3, 1+2);
