33c509bb1ff90f8bb4406868835967e823d0bed3
Steven <steven.agbassah@student.vinci.be>
2023-12-06 22:53:59 +0100

    add : javadoc clientService

--- src/main/java/vinci/be/backend/services/ClientService.java
+++ src/main/java/vinci/be/backend/services/ClientService.java
@@ -17,30 +17,44 @@ public class ClientService {
     }
 
     /**
-     * Reads all videos in repository
+     * Reads all clients in repository
      *
      * @return all videos
      */
-    @Transactional
     public List<Client> readAll() {
         return clientRepository.findAll();
     }
 
 
-    @Transactional
+    /**
+     * Retrieves a specific client from the repository based on the provided client ID.
+     *
+     * @param clientId The unique identifier of the client to be retrieved.
+     * @return The client object corresponding to the provided client ID, or null if not found.
+     */
     public Client readOne(int clientId) {
         return clientRepository.findById(clientId).orElse(null);
     }
 
 
-    @Transactional
+    /**
+     * Creates a new client in the repository if the provided client ID does not already exist.
+     *
+     * @param client The client object to be created.
+     * @return true if the client is successfully created, false if a client with the same ID already exists.
+     */
     public boolean createOne(Client client) {
         if (clientRepository.existsById(client.getClient_id())) return false;
         clientRepository.save(client);
         return true;
     }
 
-    @Transactional
+    /**
+     * Updates an existing client in the repository if the provided client ID already exists.
+     *
+     * @param client The client object with updated information.
+     * @return true if the client is successfully updated, false if the client with the provided ID does not exist.
+     */
     public boolean updateOne(Client client) {
         if (!clientRepository.existsById(client.getClient_id())) return false;
         clientRepository.save(client);
