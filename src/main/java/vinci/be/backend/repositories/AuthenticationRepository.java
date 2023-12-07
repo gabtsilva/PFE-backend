705a0a94f4172df4a3387f3821c29bf7a09643d4
Steven Agbassah <steven.agbassah@student.vinci.be>
2023-12-07 13:16:16 +0100

    correction : lien entre user et authentication

--- src/main/java/vinci/be/backend/repositories/AuthenticationRepository.java
+++ src/main/java/vinci/be/backend/repositories/AuthenticationRepository.java
@@ -1,10 +1,11 @@
 package vinci.be.backend.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import vinci.be.backend.models.SafeCredentials;
 import vinci.be.backend.models.UnsafeCredentials;
 
 @Repository
-public interface AuthenticationRepository extends CrudRepository<SafeCredentials, String> {
+public interface AuthenticationRepository extends JpaRepository<SafeCredentials, String> {
 }
