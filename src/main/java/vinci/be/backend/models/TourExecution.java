b12038c296f5e4042a1c64b71d7b72c0f6c8e7f6
CrapsDorian <116069115+CrapsDorian@users.noreply.github.com>
2023-12-08 15:33:12 +0100

    fix TourExecution

--- src/main/java/vinci/be/backend/models/TourExecution.java
+++ src/main/java/vinci/be/backend/models/TourExecution.java
@@ -1,45 +1,42 @@
-//package vinci.be.backend.models;
-//
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//import java.time.LocalDate;
-//
-//@Getter
-//@Setter
-//@ToString
-//@NoArgsConstructor
-//@Entity(name = "tours_executions")
-//@Table(name="tours_executions")
-//public class TourExecution {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//
-//    @Column(name="state", nullable=false)
-//    private String state;
-//
-//    @Column(name="delivery_person", nullable=false)
-//    private String deliveryPerson;
-//
-//    @Column(name="vehicle_id", nullable=false)
-//    private int vehicleId;
-//
-//    @Column(name="tour_id", nullable=false)
-//    private int tourId;
-//
-//    @Column(name="execution_date", nullable=false)
-//    private LocalDate executionDate;
-//
-//
-//    public boolean invalid(){
-//        return  (!state.equals(TourState.COMMENCEE.getLabel()) && !state.equals(TourState.PREVUE.getLabel()) && !state.equals(TourState.TERMINEE.getLabel()) )
-//                || deliveryPerson == null || deliveryPerson.isEmpty() || deliveryPerson.isBlank() || executionDate == null  ;
-//
-//    }
-//}
+package vinci.be.backend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity(name = "tours_executions")
+@Table(name="tours_executions")
+public class TourExecution {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name="state", nullable=false)
+  private String state;
+
+  @Column(name="delivery_person", nullable=false)
+  private String deliveryPerson;
+
+  @Column(name="vehicle_id", nullable=false)
+  private int vehicleId;
+
+  @Column(name="tour_id", nullable=false)
+  private int tourId;
+
+  @Column(name="execution_date", nullable=false)
+  private LocalDate executionDate;
+
+  public boolean invalid(){
+    return (!state.equals(TourState.COMMENCEE.getLabel()) && !state.equals(TourState.PREVUE.getLabel()) && !state.equals(TourState.TERMINEE.getLabel()))
+        || deliveryPerson == null || deliveryPerson.isEmpty() || deliveryPerson.isBlank() || executionDate == null;
+  }
+}
--- src/main/java/vinci/be/backend/repositories/TourExecutionRepository.java
+++ src/main/java/vinci/be/backend/repositories/TourExecutionRepository.java
@@ -1,20 +1,18 @@
-//package vinci.be.backend.repositories;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import vinci.be.backend.models.Tour;
-//import vinci.be.backend.models.TourExecution;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//@Repository
-//public interface TourExecutionRepository extends JpaRepository<TourExecution, Integer> {
-//    boolean existsByExecutionDateAndTourId(LocalDate executionDate, int tour);
-//
-//    List<TourExecution> findByTourIdAndState(int tourId, String state);
-//
-//    List<TourExecution> findByTourIdAndDeliveryPersonAndState(int tourId, String userMail, String state);
-//
-//}
-//
+package vinci.be.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vinci.be.backend.models.TourExecution;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface TourExecutionRepository extends JpaRepository<TourExecution, Integer> {
+  boolean existsByExecutionDateAndTourId(LocalDate executionDate, int tour);
+
+  List<TourExecution> findByTourIdAndState(int tourId, String state);
+
+  List<TourExecution> findByTourIdAndDeliveryPersonAndState(int tourId, String userMail, String state);
+
+}
